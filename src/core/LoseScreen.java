0f40f34d62587c2eee4da2ca8d9b4e4b9cd5beff
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-05-05 12:42:34 +0600

    added lose screen with all the necessary buttons

--- src/core/LoseScreen.java
+++ src/core/LoseScreen.java
@@ -1,13 +1,19 @@
 package core;
 
+import utils.Button;
+import utils.FontManager;
+
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoseScreen extends Screen {
-    private JButton replay;
-    private JButton goToMain;
+    private JButton replayBtn;
+    private JButton quitBtn;
     protected JLabel answerLabel;
+    private Image loseImage;
 
     public LoseScreen(Window window){
         super(window);
@@ -18,23 +24,53 @@ public class LoseScreen extends Screen {
 
     @Override
     public void initialize() {
-        JLabel winLabel = new JLabel("You Lost :(");
-        JLabel LosingScore=new JLabel("Your Score is 0");
-        answerLabel = new JLabel();
-        replay = new JButton("Replay?");
-        goToMain=new JButton("Go To Main Menu");
 
+
+
+        loseImage = new ImageIcon("src/resources/lose_screen.gif").getImage();
+
+        ImageIcon replayIcon = new ImageIcon("./src/resources/btn_replay.png");
+        replayBtn = Button.createButton(replayIcon);
+        replayBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        ImageIcon quitIcon = new ImageIcon("./src/resources/btn_quit.png");
+        quitBtn = Button.createButton(quitIcon);
+        quitBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        //to show the correct answer
+        Font KGPrimary = FontManager.loadFont(
+                "./src/resources/KGPrimaryPenmanship.ttf",
+                Font.BOLD, 25f
+        );
+        answerLabel = new JLabel();
+        answerLabel.setFont(KGPrimary);
+
+
+        this.add(replayBtn);
+        this.add(quitBtn);
         this.add(answerLabel);
-        this.add(winLabel);
-        this.add(replay);
-        this.add(goToMain);
-        this.add(LosingScore);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        //gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(350, 0, 0, 0);
+        add(answerLabel, gbc);
+
+        gbc.gridy++;
+        gbc.insets = new Insets(20, 0, 0, 0);
+        add(replayBtn, gbc);
+
+        gbc.gridy++;
+        gbc.insets = new Insets(20, 0, 0, 0);
+        add(quitBtn, gbc);
 
     }
 
     @Override
     public void update() {
-        replay.addActionListener(new ActionListener() {
+        replayBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 // go back to game screen
@@ -45,12 +81,21 @@ public class LoseScreen extends Screen {
                 window.gameScreen.getWordListFile().generateWord();
             }
         });
-        goToMain.addActionListener(new ActionListener() {
+
+        quitBtn.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                window.changeScreen(ScreenType.WELCOME);
+            public void actionPerformed(ActionEvent actionEvent) {
+                // release all resources and close the app
+                window.dispose();
             }
         });
     }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
+        g.drawImage(loseImage, 0, 0, getWidth(), getHeight(), this);
+    }
 }
+
+
